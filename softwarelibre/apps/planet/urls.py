b318d6cd193f4a4c193811ae9e4219fb229c7f20
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2008-12-31 16:58:27 -0600

    corrigiendo error en planet.urls

--- softwarelibre/apps/planet/urls.py
+++ softwarelibre/apps/planet/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
      (r'^$', 'softwarelibre.apps.planet.views.index'),
-     (r'^/autor/(?P<author>)/$', 'softwarelibre.apps.planet.views.filter_by_author')
-     #(r'^/articulo/(?P<item>)/$', 'softwarelibre.apps.planet.views.show_item')
+     (r'^autor/(?P<author>[A-Za-z0-9]+(?:\+[A-Za-z0-9]+)*)/$', 'softwarelibre.apps.planet.views.filter_by_author')
+     #(r'^articulo/(?P<item>[A-Za-z0-9]+(?:\+[A-Za-z0-9]+)*)/$', 'softwarelibre.apps.planet.views.show_item')
 )
